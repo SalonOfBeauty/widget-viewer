b3991bbde56e1af11631fe4c353db8065c6e38cd
av.sherchkov <av.sherchkov@tensor.ru>
2022-01-11 14:09:40 +0500

    Готово

--- handlers.js
+++ handlers.js
@@ -1,3 +1,11 @@
 function clickHandler() {
-    alert('Клик!');
+    const input = document.getElementById('input');
+    const iframe = document.getElementById('iframe');
+    const inputValue = input.value;
+
+    if (inputValue === iframe.src) {
+        return;
+    }
+
+    iframe.src = input.value;
 };
